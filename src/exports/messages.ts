80886f93c7d54355eb63d5be907ac96d5507d20e
Your Name <you@example.com>
2024-12-08 22:14:36 -0300

    up

---
+++ src/exports/messages.ts
@@ -0,0 +1,27 @@
+
+
+export const extractMessage = (messageDetails: any) => {
+    if (!messageDetails ||  !messageDetails.message) {
+        return{
+            media: null,
+            text: null,
+            args: [],
+            fullMessage: null,
+            mentions: null,
+            from: null,
+            fromUser: null,
+            isGroupMsg: null,
+            isCommand: null,
+            commandName: null,
+            userName: null,
+            participant: null,
+
+        }
+    }
+
+    const textMessage = messageDetails.message?.conversation || ""
+    const extendedTextMessage = messageDetails.message?.extendedTextMessage?.text || ""
+    const 
+
+
+}
